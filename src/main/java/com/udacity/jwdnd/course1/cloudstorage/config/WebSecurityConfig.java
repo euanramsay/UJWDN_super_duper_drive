dbd157e447c28cc81057cea7c54674e5393e8471
Euan Ramsay <euan.ramsay@ros.gov.uk>
2020-12-18 16:01:09 +0000

    Add h2 config.

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/WebSecurityConfig.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/WebSecurityConfig.java
@@ -26,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests()
-                .antMatchers("/signup", "/login", "/css/**", "/js/**")
+                .antMatchers("/signup", "/h2-console", "/login", "/css/**", "/js/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
